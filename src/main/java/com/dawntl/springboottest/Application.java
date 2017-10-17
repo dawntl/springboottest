381150f28dba68b38f3f2c8c9347182049de8f17
liangxiong <liangxiong@liangxiongdeMac-mini.local>
2017-10-17 09:55:53 +0800

    新项目提交

---
+++ src/main/java/com/dawntl/springboottest/Application.java
@@ -0,0 +1,25 @@
+package com.dawntl.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String test() {
+        return "Hello Word!";
+    }
+
+}
